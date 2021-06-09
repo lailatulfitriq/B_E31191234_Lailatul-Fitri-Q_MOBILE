8d9db7bc46ea4bf394d908711250e521210f6482
lailatulfitriq <lfitri1012@gmail.com>
2021-06-09 15:28:53 +0700

    API Volley

---
+++ APIVolley_Fitri/app/src/main/java/com/example/apivolley_fitri/Util/ServerAPI.java
@@ -0,0 +1,8 @@
+package com.example.apivolley_fitri.Util;
+
+public class ServerAPI {
+    public static final String URL_DATA = "http://192.168.100.7/Mahasiswa/Api";
+    public static final String URL_INSERT = "http://192.168.100.7/Mahasiswa/ApiInsert";
+    public static final String URL_DELETE = "http://192.168.100.7/Mahasiswa/ApiDelete";
+    public static final String URL_UPDATE = "http://192.168.100.7/Mahasiswa/ApiUpdate";
+}
